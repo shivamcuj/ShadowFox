eadf4f3252d27ee9012b2f219339d2611c214423
Shivam Kumar <shivamjnv2305@gmail.com>
2026-06-25 16:27:59 +0530

    Create 02_numbers.py

---
+++ Python Development Internship June 2026/Beginner Level/2.Numbers/02_numbers.py
@@ -0,0 +1,6 @@
+# This program calculates the area of a pond and the total water in it based on the radius.
+radius = 84
+area = 3.14 * radius**2 #area of the pond
+print(f"Area of the pond = {area}m^2")
+Total_water = 1.4 * area
+print(f"Total water in the pond = {Total_water}l")
